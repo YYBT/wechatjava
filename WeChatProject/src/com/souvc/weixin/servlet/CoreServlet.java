28227d71b7ab0d146bb0057f0625233ba5f56bd3
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2017-03-31 13:22:54 +0800

    提交

---
+++ WeChatProject/src/com/souvc/weixin/servlet/CoreServlet.java
@@ -0,0 +1,93 @@
+package com.souvc.weixin.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.souvc.weixin.util.SignUtil;
+
+import demo.process.WechatProcess;
+
+/**
+ * 类名: CoreServlet </br> 
+ * 描述: 来接收微信服务器传来信息 </br> 
+ * 开发人员： souvc</br> 
+ * 创建时间：2015-9-29 </br> 
+ * 发布版本：V1.0 </br>
+ */
+public class CoreServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 4323197796926899691L;
+
+    /**
+     * 确认请求来自微信服务器
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // 微信加密签名
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+
+        PrintWriter out = response.getWriter();
+        
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            out.print(echostr);
+        }
+        
+        out.close();
+        out = null;
+    }
+
+    /**
+     * 处理微信服务器发来的消息
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO 消息的接收、处理、响应
+    	
+    	request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		/** 读取接收到的xml消息 */
+		StringBuffer sb = new StringBuffer();
+		InputStream is = request.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String s = "";
+		
+		while ((s = br.readLine()) != null) {
+			sb.append(s);
+		}
+		String xml = sb.toString();	//次即为接收到微信端发送过来的xml数据
+
+		String result = "";
+			//正常的微信处理流程
+		result = new WechatProcess().processWechatMag(xml);
+	
+		try {
+
+			 PrintWriter out = response.getWriter();
+		        out.print(result);
+		        out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+    }
+
+}
